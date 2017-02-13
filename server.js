db66b15c7c454fcfcb48639c850f870b6b978004
Deepak kumar sahoo <deepaksahoo9725@gmail.com>
2017-02-13 23:07:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/article-two',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-three',function(req,res){
-    res.send('Article three requested and will be served here');
+    res.sendFile(path.join(__dirname, 'ui', 'three.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
