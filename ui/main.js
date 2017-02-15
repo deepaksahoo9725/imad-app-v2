a7d6ab83bc117d15e6dcdf09d078708af7217124
Deepak kumar sahoo <deepaksahoo9725@gmail.com>
2017-02-15 21:11:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //change the text of html
 var element = document.getElementById('main-text');
 element.innerHTML = 'NEW VALUE';
+//move the image
+var img = document.getElementById('madi');
+img.onclick = function(){
+  img.style.marginLeft='100px';  
+};
