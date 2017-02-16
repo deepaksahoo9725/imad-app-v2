dc3459d8be364cad0fb73fc2ee41a726b58cf82b
Deepak kumar sahoo <deepaksahoo9725@gmail.com>
2017-02-16 15:31:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,9 +5,12 @@ element.innerHTML = 'NEW VALUE';
 //move the image
 var img = document.getElementById('madi');
 var marginLeft =0;
+var borderTop = 0;
 function moveRight(){
     marginLeft = marginLeft+0.5;
+    borderTop = borderTop+1;
     img.style.marginLeft = marginLeft + 'px';
+    img.style.borderTop = borderTop + 'px';
 }
 
 img.onclick = function(){
